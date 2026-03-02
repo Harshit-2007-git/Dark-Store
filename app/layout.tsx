f211dc6fc0cd3ec920f8bd1f28ba44740a60cbc1
Harshit-2007-git <harshittiwari2226@gmail.com>
2026-03-02 23:23:37 +0530

    Import GradientBlinds component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import GradientBlinds from "@/components/GradientBlinds";
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
