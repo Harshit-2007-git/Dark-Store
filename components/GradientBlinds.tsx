d1d6dfecfc228cb30bc490e10f4cf50fa28bd93c
Harshit-2007-git <harshittiwari2226@gmail.com>
2026-03-02 23:22:48 +0530

    Add GradientBlinds component usage in GradientBlinds.tsx

---
+++ components/GradientBlinds.tsx
@@ -0,0 +1,19 @@
+'use client';
+import GradientBlinds from './GradientBlinds';
+
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <GradientBlinds
+    gradientColors={['#FF9FFC', '#5227FF']}
+    angle={0}
+    noise={0.3}
+    blindCount={12}
+    blindMinWidth={50}
+    spotlightRadius={0.5}
+    spotlightSoftness={1}
+    spotlightOpacity={1}
+    mouseDampening={0.15}
+    distortAmount={0}
+    shineDirection="left"
+    mixBlendMode="lighten"
+  />
+</div>
